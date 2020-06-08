a9b4ab37261244622b1baa5e79ec002f436e65d9
flipmcf <flipmcf@gmail.com>
2020-06-08 14:13:02 -0400

    pipeline for rfa

--- src/rfaplone4to5/migration/import/blueprints.py
+++ src/rfaplone4to5/migration/import/blueprints.py
@@ -15,6 +15,90 @@ logger = logging.getLogger('Transmogrifier')
 
 @implementer(ISection)
 @provider(ISectionBlueprint)
+class Skipper(object):
+    """Skip Plonesite, subsites folder, and subsite
+       They will (have been) already created
+    """
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+
+    def __iter__(self):
+        for item in self.previous:
+            if item['_path'] in ("/rfa",
+                                 "/rfa/subsites"):
+                logger.info('[SKIPPING] %s', item['_path'])
+                continue
+            if item['_type'] == "Subsite":
+                logger.info('[SKIPPING Subsite] %s', item['_path'])
+                continue
+            
+            yield item
+            
+            
+@implementer(ISection)
+@provider(ISectionBlueprint)
+class ContentTypeMapper(object):
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+
+    def __iter__(self):
+        for item in self.previous:
+            type_maps = {'Section': 'section',
+                         'Story': 'story',
+                         'AudioClip': 'Audio Clip', 
+                         }
+            if type_maps.get(item['_type']):
+                logger.info('[MAPPING] %s to %s', item['_type'], type_maps[item['_type']])
+                item['_type'] = type_maps[item['_type']]
+                
+            yield item
+            
+@implementer(ISection)
+@provider(ISectionBlueprint)
+class Debugger(object):
+    """A Debugger Blueprint to stick in the pipeline somewhere
+    """
+    def __init__(self, transmogrifier, name, options, previous):
+        self.transmogrifier = transmogrifier
+        self.name = name
+        self.options = options
+        self.previous = previous
+        self.context = transmogrifier.context
+        
+        if 'path-key' in options:
+            pathkeys = options['path-key'].splitlines()
+        else:
+            pathkeys = defaultKeys(options['blueprint'], name, 'path')
+        self.pathkey = Matcher(*pathkeys)
+        
+    def __iter__(self):
+        for item in self.previous:
+            if item['_path'] == '/rfa/subsites/lao/Multimedia/LaoGame-12302010114149.html/talkback/1374893596':
+                import pdb; pdb.set_trace()
+            
+            yield item
+
+
+
+
+
+
+
+
+
+
+            
+@implementer(ISection)
+@provider(ISectionBlueprint)
 class Example(object):
     """An example blueprint.
     """
