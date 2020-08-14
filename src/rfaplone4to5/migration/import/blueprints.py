6ef33342df335ed68ec2d11b1af778dbaecaa1ec
flipmcf <flipmcf@gmail.com>
2020-08-14 13:32:36 -0400

    fix badpaths

--- src/rfaplone4to5/migration/import/blueprints.py
+++ src/rfaplone4to5/migration/import/blueprints.py
@@ -51,7 +51,6 @@ class Skipper(object):
                 '/rfa/subsites/english/Test-for-Minh-Ha/banner/content/images/image7.jpg',
                 '/rfa/subsites/english/Test-for-Minh-Ha/banner/content/images/image8.jpg',
                 '/rfa/subsites/english/news/myanmar/aung-san-suu-kyi/aung-san-suu-kyi',
-                '/rfa/subsites/english/news/vietnam/patriarch-07072008222314.html', # linked captioned image
     )
 
 
